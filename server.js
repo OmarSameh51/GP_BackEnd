4099bae8c1dd41d6fc2e9dbaa6db26cdb7394af1
Omar Sameh <omarsameh51@outlook.com>
2026-05-20 21:00:20 +0300

    adj port

--- server.js
+++ server.js
@@ -69,7 +69,7 @@ app.use((err, req, res, next) => {
   });
 });
 
-const PORT = process.env.PORT || 9000;
+const PORT = process.env.PORT || 5000;
 
 if (require.main === module) {
   app.listen(PORT, () => {
