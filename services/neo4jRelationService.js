b19acb50db64c0e9b7de183b9a142586c0fa003d
Omar Sameh <omarsameh51@outlook.com>
2026-06-05 18:34:38 +0300

    Created neo4jRelationService.js

---
+++ services/neo4jRelationService.js
@@ -0,0 +1,79 @@
+const { driver } = require("../config/neo4j");
+
+const createTookRelation = async (studentId, courseCode) => {
+  const session = driver.session();
+
+  try {
+    await session.run(
+      `
+      MATCH (s:Student {studentId: $studentId})
+      MATCH (c:Course {Code: $courseCode})
+
+      MERGE (s)-[:TOOK]->(c)
+      `,
+      {
+        studentId,
+        courseCode,
+      },
+    );
+  } finally {
+    await session.close();
+  }
+};
+
+const deleteTookRelation = async (studentId, courseCode) => {
+  const session = driver.session();
+
+  try {
+    await session.run(
+      `
+      MATCH (s:Student {studentId: $studentId})
+            -[r:TOOK]->
+            (c:Course {Code: $courseCode})
+
+      DELETE r
+      `,
+      {
+        studentId,
+        courseCode,
+      },
+    );
+  } finally {
+    await session.close();
+  }
+};
+
+const updateIntendsRelation = async (studentId, preferredDepartment) => {
+  const session = driver.session();
+
+  try {
+    await session.run(
+      `
+      MATCH (s:Student {studentId: $studentId})
+
+      OPTIONAL MATCH (s)-[r:INTENDS]->(:Department)
+      DELETE r
+
+      WITH s
+
+      MATCH (d:Department {
+  code: $preferredDepartment
+})
+
+      MERGE (s)-[:INTENDS]->(d)
+      `,
+      {
+        studentId,
+        preferredDepartment,
+      },
+    );
+  } finally {
+    await session.close();
+  }
+};
+
+module.exports = {
+  createTookRelation,
+  deleteTookRelation,
+  updateIntendsRelation,
+};
