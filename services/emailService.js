487f139607590d76a8a5b624bf62c8a0fd0f01b0
Omar Sameh <omarsameh51@outlook.com>
2026-06-09 10:39:50 +0300

    debug brevo

--- services/emailService.js
+++ services/emailService.js
@@ -1,43 +1,9 @@
 const brevo = require("@getbrevo/brevo");
 
-const client = new brevo.Brevo({
-  apiKey: process.env.BREVO_API_KEY,
-});
+console.log("BREVO KEYS:");
+console.log(Object.keys(brevo));
 
-const sendVerificationEmail = async (email, code) => {
-  try {
-    console.log("Sending email to:", email);
+console.log("BREVO.Brevo KEYS:");
+console.log(Object.keys(brevo.Brevo));
 
-    await client.transactionalEmails.sendTransacEmail({
-      sender: {
-        email: process.env.EMAIL_USER,
-        name: "Academic Advisor",
-      },
-
-      to: [
-        {
-          email,
-        },
-      ],
-
-      subject: "Verify Your Email",
-
-      htmlContent: `
-        <h2>Email Verification</h2>
-        <p>Your verification code is:</p>
-        <h1>${code}</h1>
-        <p>This code expires in 10 minutes.</p>
-      `,
-    });
-
-    console.log("Email sent successfully");
-  } catch (error) {
-    console.error("BREVO ERROR:");
-    console.error(error);
-    throw error;
-  }
-};
-
-module.exports = {
-  sendVerificationEmail,
-};
+module.exports = {};
