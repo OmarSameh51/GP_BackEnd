f3d5712b05a451ab4cd771df1b35962cc2ff6632
Omar Sameh <omarsameh51@outlook.com>
2026-06-09 11:14:34 +0300

    inspect transactional emails

--- services/emailService.js
+++ services/emailService.js
@@ -5,5 +5,6 @@ console.log(Object.keys(brevo));
 
 console.log("BREVO.Brevo KEYS:");
 console.log(Object.keys(brevo.Brevo)); //test
-
+console.log("transactionalEmails:");
+console.log(brevo.Brevo.transactionalEmails);
 module.exports = {};
